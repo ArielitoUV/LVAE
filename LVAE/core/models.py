c18d5e0d4a84f9a530c9d4e7ccfb229d2fe2cb15
Ariel <ariel@gmail.com>
2023-11-23 22:36:39 -0300

    s

--- LVAE/core/models.py
+++ LVAE/core/models.py
@@ -1,12 +1,1 @@
 from django.db import models
-
-
-class Registro(models.Model):
-    nombre = models.CharField(max_length=100)
-    apellido = models.CharField(max_length=100)
-    estado = models.CharField(max_length=100)
-    ciudad = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=15)
-
-    def __str__(self):
-        return f"{self.nombre} {self.apellido}"
