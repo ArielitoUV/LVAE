3c34ef48ef5131f26b46cde9fa0937dcc9fd9252
Ariel <ariel@gmail.com>
2023-11-29 22:36:48 -0300

    a

--- LVAE/core/forms.py
+++ LVAE/core/forms.py
@@ -7,11 +7,12 @@ from django import forms
 
 
 
+
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(
         label=_('Correo electrónico'),
-        widget=forms.EmailInput(attrs={'autofocus': False,'class':'form-control', 'placeholder': 'Ingrese corrreo electronico'}),
-        )
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese correo electrónico'}),
+    )
     nombre = forms.CharField(
         label=_("Nombre"),
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su nombre'}),
@@ -33,9 +34,9 @@ class CustomUserCreationForm(UserCreationForm):
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su Ciudad'}),
     )
     user_type = forms.ChoiceField(
-    label=_("Tipo de Usuario"),
-    choices=[('', 'Seleccione una opción'), ('apoderado', 'Apoderado'), ('alumno', 'Alumno')],
-    widget=forms.Select(attrs={'class': 'form-control'}),
+        label=_("Tipo de Usuario"),
+        choices=[('', 'Seleccione una opción'), ('apoderado', 'Apoderado'), ('alumno', 'Alumno')],
+        widget=forms.Select(attrs={'class': 'form-control'}),
     )
 
     class Meta:
@@ -49,11 +50,14 @@ class CustomUserCreationForm(UserCreationForm):
         self.error_messages['password_common'] = _("La contraseña no puede ser una contraseña común.")
         self.error_messages['password_entirely_numeric'] = _("La contraseña no puede ser completamente numérica.")
         self.error_messages['email_in_use'] = _('Este email ya está en uso. Por favor, elige otro.')
-        self.fields['user_type'].widget.attrs['readonly'] = True
+
+        # Aplica clases de Bootstrap a los campos con errores
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
 
 
 class CustomAuthenticationForm(AuthenticationForm):
-    username = forms.EmailField(label=_('Correo electrónico'), widget=forms.EmailInput(attrs={'autofocus': False}))
+    username = forms.EmailField(label=_('Correo electrónico'), widget=forms.EmailInput(attrs={'autofocus': False,'placeholder':'Ingrese correo electrónico'}))
     password = forms.CharField(
         label=_("Contraseña"),
         strip=False,
