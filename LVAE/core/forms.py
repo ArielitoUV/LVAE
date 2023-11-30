7830100325c1919ba9e3a6c38ccaf4b46ae3ed1c
Ariel <ariel@gmail.com>
2023-11-30 02:38:36 -0300

    S

--- LVAE/core/forms.py
+++ LVAE/core/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.utils.translation import gettext_lazy as _
-from .models import Usuario
+from .models import Usuarios
 from django import forms
 
 
@@ -39,7 +39,7 @@ class CustomUserCreationForm(UserCreationForm):
     )
     
     class Meta:
-        model = Usuario
+        model = Usuarios
         fields = ['email', 'nombre', 'apellido', 'telefono', 'estado', 'ciudad','user_type', 'password1', 'password2']
 
     def __init__(self, *args, **kwargs):
