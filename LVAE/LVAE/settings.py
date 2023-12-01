26e2506db8854392ee4c0f2f4adee3a528e7a66b
Ariel <ariel@gmail.com>
2023-12-01 16:01:22 -0300

    aa

--- LVAE/LVAE/settings.py
+++ LVAE/LVAE/settings.py
@@ -74,25 +74,19 @@ WSGI_APPLICATION = 'LVAE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#
-#    }
-#}
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE' : 'django.db.backends.mysql',
-#          'NAME' : 'viaalexito$exito',
-#          'USER' : 'viaalexito',
-#          'PASSWORD' : 'rooT9876543210',
-#          'HOST' : 'viaalexito.mysql.pythonanywhere-services.com',
-#          'PORT' : '3306',
 
-#      }
-#  }
+DATABASES = {
+     'default': {
+         'ENGINE' : 'django.db.backends.mysql',
+         'NAME' : 'viaalexito$exito',
+         'USER' : 'viaalexito',
+         'PASSWORD' : 'rooT9876543210',
+         'HOST' : 'viaalexito.mysql.pythonanywhere-services.com',
+         'PORT' : '3306',
+
+     }
+ }
 
 
 # Password validation
