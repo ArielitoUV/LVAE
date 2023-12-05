402d77df2fcb4725abb843c54b2ebfebbd134063
Ariel <ariel@gmail.com>
2023-12-05 00:09:32 -0300

    asasas

--- LVAE/core/admin.py
+++ LVAE/core/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
-admin.site.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    list_display = ('email', 'nombre', 'apellido', 'user_type', 'is_active')
+    list_filter = ('user_type', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Información personal', {'fields': ('nombre', 'apellido', 'fecha_nacimiento', 'telefono', 'estado', 'ciudad')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff')}),
+        ('Tipo de usuario', {'fields': ('user_type',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nombre', 'apellido', 'fecha_nacimiento', 'telefono', 'estado', 'ciudad', 'password1', 'password2', 'user_type'),
+        }),
+    )
+
+admin.site.register(Usuario, UsuarioAdmin)
